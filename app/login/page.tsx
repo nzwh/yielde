1ec3146c0b0aa3a5fcef4e4d005ea8aee6808519
nzwh <notzerowh@gmail.com>
2026-09-05 14:06:36 +0800

    feat(routing): add login/register pages and route protection

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+import { Suspense } from "react";
+import LoginCard from "@/components/LoginCard/LoginCard";
+import { FadeIn } from "@/components/global/FadeFlyIn";
+
+export default function LoginPage() {
+  return (
+    <main className="flex h-full w-full items-center justify-center bg-white text-black">
+      <Suspense fallback={null}>
+        <FadeIn direction="up" delay={200} elastic>
+          <LoginCard />
+        </FadeIn>
+      </Suspense>
+    </main>
+  );
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import { FadeIn } from "@/components/global/FadeFlyIn";
+import RegisterCard from "@/components/RegisterCard/RegisterCard";
+
+export default function Home() {
+  return (
+    <main className="flex h-full w-full items-center justify-center bg-white text-black">
+      <FadeIn direction="up" delay={100} elastic>
+        <RegisterCard />
+      </FadeIn>
+    </main>
+  );
+}
