6794e03a31226b36ab67902ea86b0200cbf705c5
nzwh <notzerowh@gmail.com>
2026-09-08 22:21:49 +0800

    feat(ui): add redirect link to home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import { Suspense } from "react";
 import LoginCard from "@/components/LoginCard/LoginCard";
 import { FadeIn } from "@/components/global/FadeFlyIn";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-dvh max-w-screen items-center justify-center overflow-hidden p-4 text-black">
+    <main className="flex min-h-dvh max-w-screen flex-col items-center justify-center gap-8 overflow-hidden p-4 text-black">
       <Suspense fallback={null}>
-        <FadeIn direction="up" delay={200} elastic className="w-full max-w-88">
+        <FadeIn delay={200} elastic className="w-full max-w-88">
           <LoginCard />
+        </FadeIn>
+
+        <FadeIn delay={300} elastic className="text-xs text-[#707070]">
+          <Link href="/" aria-label="Go to Home" className="hover:underline">
+            Spin without an account
+          </Link>
         </FadeIn>
       </Suspense>
     </main>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { FadeIn } from "@/components/global/FadeFlyIn";
 import RegisterCard from "@/components/RegisterCard/RegisterCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-dvh max-w-screen items-center justify-center overflow-hidden bg-white p-4 text-black">
-      <FadeIn direction="up" delay={100} elastic className="w-full max-w-88">
+    <main className="flex min-h-dvh max-w-screen flex-col items-center justify-center gap-8 overflow-hidden p-4 text-black">
+      <FadeIn delay={100} className="w-full max-w-88" elastic>
         <RegisterCard />
+      </FadeIn>
+
+      <FadeIn delay={300} className="text-xs text-[#707070]" elastic>
+        <Link href="/" aria-label="Go to Home" className="hover:underline">
+          Spin without an account
+        </Link>
       </FadeIn>
     </main>
   );
