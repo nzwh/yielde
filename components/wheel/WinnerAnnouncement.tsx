dbfc246073ff18978177255af0d534b493e12341
nzwh <notzerowh@gmail.com>
2026-09-08 12:15:48 +0800

    refactor(ui): fix typo

--- components/wheel/WinnerAnnouncement.tsx
+++ components/wheel/WinnerAnnouncement.tsx
@@ -57,7 +57,7 @@ export function WinnerAnnouncement({
       elastic
     >
       <p>
-        Press<Badge>Space</Badge>or<Badge>Tap</Badge>the to Spin
+        Press<Badge>Space</Badge>or<Badge>Tap the Wheel</Badge>to Spin
       </p>
     </FadeIn>
   );
