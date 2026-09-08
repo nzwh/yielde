9ed5a9909f75b2dce356636c389f5b9afca6d199
nzwh <notzerowh@gmail.com>
2026-09-08 15:06:34 +0800

    fix(ui): add focus-visible ring to custom checkbox

--- components/global/Checkbox.tsx
+++ components/global/Checkbox.tsx
@@ -24,7 +24,7 @@ export function Checkbox({
         onChange={(e) => onChange(e.target.checked)}
         className={cn(
           "border-[#EDEDED] bg-white checked:bg-[#798BFF] hover:bg-[#B7B7B7] hover:checked:bg-[#3B4ACF]",
-          "peer size-3.5 cursor-pointer appearance-none rounded-sm border transition-all duration-200 ease-in-out",
+          "peer size-3.5 cursor-pointer appearance-none rounded-sm border transition-all duration-200 ease-in-out focus-visible:ring-1 focus-visible:ring-[#8290EF] focus-visible:outline-none",
         )}
       />
       <MdCheck className="pointer-events-none absolute inset-0 size-3.5 text-white opacity-0 peer-checked:opacity-100" />
